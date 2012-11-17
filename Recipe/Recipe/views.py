4a9302fe96d6cbc75eb47166c60abf188f7bd6a0
Subhodip <subhodipbiswas@gmail.com>
2012-11-17 15:44:11 +0530

    Working admin model

---
+++ Recipe/Recipe/views.py
@@ -0,0 +1,29 @@
+# Create your views here.
+#from models import *
+from django.shortcuts import render_to_response, get_object_or_404
+from ox.django.shortcuts import get_object_or_404_json, render_to_json_response
+
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from fuzzywuzzy import process as fuzzprocess
+from django.http import HttpResponse
+
+
+def index(request):
+    return render_to_response('index.html', context)
+
+def about(request):
+    context = RequestContext(request, {})
+    return render_to_response("innov/about_chalobest.html", context)
+
+
+def join_us(request):
+    context = RequestContext(request, {})
+    return render_to_response("innov/joinus.html", context)
+
+
+def contact(request):
+    context = RequestContext(request, {})
+    return render_to_response("innov/contactus.html", context)
+
---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!/home/subhodip/Project/Recipe/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
