5c742197dd97c3c866594a9819daa8f81552b744
Subhodip <subhodipbiswas@gmail.com>
2012-11-19 21:31:04 +0530

    refactoring

--- Recipe/chef/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from chef.models import RecipePoster
-
-
-class RecipeAdmin(admin.ModelAdmin):
-	fields =['created_date', 'dish_name', 'dish_desc', 'course_type', 'cusine_type', 'dish_ingre', 'dish_method','photo']
-admin.site.register(RecipePoster, RecipeAdmin)
