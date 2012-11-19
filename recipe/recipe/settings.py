d1e664983cb0144fa0d3336eb1c1045c20c02861
Subhodip <subhodipbiswas@gmail.com>
2012-11-19 22:00:11 +0530

    fixed minor errors

--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -21,8 +21,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': join(PROJECT_ROOT, 'chalobest.db'),                      # Or path to database file if using sqlite3.
-        'USER': 'sanj',                      # Not used with sqlite3.
+        'NAME': join(PROJECT_ROOT, 'sample.db'),                      # Or path to database file if using sqlite3.
+        'USER': 'user',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -122,7 +122,7 @@ MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware'
 )
 
-ROOT_URLCONF = 'Recipe.urls'
+ROOT_URLCONF = 'recipe.urls'
 
 TEMPLATE_DIRS = (
     join(PROJECT_ROOT, 'templates'),    
